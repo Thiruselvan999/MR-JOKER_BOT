faccdb6eecb2038b3183a64e6769a6cd859ae014
ValtAoiTheBot <91861970+ValtAoiTheBot@users.noreply.github.com>
2021-11-18 14:52:03 +0530

    Update __init__.py

--- mrjoker/__init__.py
+++ mrjoker/__init__.py
@@ -1,4 +1,3 @@
-
 import logging
 import os
 import sys
@@ -98,273 +97,154 @@ if ENV:
     CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
     AI_API_KEY = os.environ.get("AI_API_KEY", None)
-    
-
     #STRING_SESSION = os.environ.get("STRING_SESSION", None)
-
+    BOT_NAME = os.environ.get("BOT_NAME", True) # Name Of your Bot.
     WALL_API = os.environ.get("WALL_API", None)
-
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
-
     SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None)
-
     SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
-
     GENIUS_API_TOKEN = os.environ.get("GENIUS_API_TOKEN", None)
-
     ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
-
     LASTFM_API_KEY = os.environ.get("LASTFM_API_KEY", None)
 
     try:
-
         BL_CHATS = set(int(x) for x in os.environ.get("BL_CHATS", "").split())
-
     except ValueError:
-
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 else:
-
     from mrjoker.config import Development as Config
 
     TOKEN = Config.TOKEN
 
     try:
-
         OWNER_ID = int(Config.OWNER_ID)
-
     except ValueError:
-
         raise Exception("Your OWNER_ID variable is not a valid integer.")
 
     JOIN_LOGGER = Config.JOIN_LOGGER
-
     OWNER_USERNAME = Config.OWNER_USERNAME
-
     ALLOW_CHATS = Config.ALLOW_CHATS
-
     try:
-
         DRAGONS = set(int(x) for x in Config.DRAGONS or [])
-
         DEV_USERS = set(int(x) for x in Config.DEV_USERS or [])
-
         WHITELIST_USERS = set(int(x) for x in Config.WHITELIST_USERS or [])
-
         SUDO_USERS = set(int(x) for x in Config.SUDO_USERS or [])
-
     except ValueError:
-
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
     try:
-
         DEMONS = set(int(x) for x in Config.DEMONS or [])
-
     except ValueError:
-
         raise Exception("Your support users list does not contain valid integers.")
 
     try:
-
         WOLVES = set(int(x) for x in Config.WOLVES or [])
-
     except ValueError:
-
         raise Exception("Your whitelisted users list does not contain valid integers.")
 
     try:
-
         TIGERS = set(int(x) for x in Config.TIGERS or [])
-
     except ValueError:
-
         raise Exception("Your tiger users list does not contain valid integers.")
 
     EVENT_LOGS = Config.EVENT_LOGS
-
     WEBHOOK = Config.WEBHOOK
-
     URL = Config.URL
-
     PORT = Config.PORT
-
     CERT_PATH = Config.CERT_PATH
-
     API_ID = Config.API_ID
-
     API_HASH = Config.API_HASH
 
     BOT_USERNAME = Config.BOT_USERNAME
-
     DB_URI = Config.SQLALCHEMY_DATABASE_URI
-
     MONGO_DB_URI = Config.MONGO_DB_URI
-
     HEROKU_API_KEY = Config.HEROKU_API_KEY
-
     HEROKU_APP_NAME = Config.HEROKU_APP_NAME
-
     TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
-
     OPENWEATHERMAP_ID = Config.OPENWEATHERMAP_ID
-
     BOT_ID = Config.BOT_ID
-
     VIRUS_API_KEY = Config.VIRUS_API_KEY
-
     DONATION_LINK = Config.DONATION_LINK
-
     LOAD = Config.LOAD
-
     NO_LOAD = Config.NO_LOAD
-
     DEL_CMDS = Config.DEL_CMDS
-
     STRICT_GBAN = Config.STRICT_GBAN
-
     WORKERS = Config.WORKERS
-
     BAN_STICKER = Config.BAN_STICKER
-
     ALLOW_EXCL = Config.ALLOW_EXCL
-
     CASH_API_KEY = Config.CASH_API_KEY
-
     TIME_API_KEY = Config.TIME_API_KEY
-
     AI_API_KEY = Config.AI_API_KEY
-
     WALL_API = Config.WALL_API
-
     SUPPORT_CHAT = Config.SUPPORT_CHAT
-
     SPAMWATCH_SUPPORT_CHAT = Config.SPAMWATCH_SUPPORT_CHAT
-
     SPAMWATCH_API = Config.SPAMWATCH_API
-
     INFOPIC = Config.INFOPIC
-
     REDIS_URL = Config.REDIS_URL
-
     ARQ_API_URL = Config.ARQ_API_URL
-
     ARQ_API_KEY = Config.ARQ_API_KEY
-
     REM_BG_API_KEY = Config.REM_BG_API_KEY
-
     GENIUS_API_TOKEN = Config.GENIUS_API_TOKEN
-
     LASTFM_API_KEY = Config.LASTFM_API_KEY
-
     #VIRUS_API_KEY = Config.VIRUS_API_KEY
-
     #STRING_SESSION = Config.STRING_SESSION
-
     
-
     try:
-
         BL_CHATS = set(int(x) for x in Config.BL_CHATS or [])
-
     except ValueError:
-
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 DRAGONS.add(OWNER_ID)
-
 DEV_USERS.add(OWNER_ID)
-
 DEV_USERS.add(1131653685)
-
 DEV_USERS.add(1107959621)
-
 WHITELIST_USERS.add(OWNER_ID)
-
 WHITELIST_USERS.add(1107959621)
-
 SUDO_USERS.add(1107959621)
-
 SUDO_USERS.add(OWNER_ID)
+
 if not SPAMWATCH_API:
-
     sw = None
-
     LOGGER.warning("SpamWatch API key missing! recheck your config.")
-
 else:
-
     try:
-
         sw = spamwatch.Client(SPAMWATCH_API)
-
     except:
-
         sw = None
-
         LOGGER.warning("Can't connect to SpamWatch!")
-
           
-
         
-
          
-
 #install aiohttp session
-
 print("[MrJoker]: Initializing AIOHTTP Session")
-
 aiohttpsession = ClientSession()    
-
     
-
 #install arq
-
 print("[MrJoker]: Initializing ARQ Client")
-
 arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)        
-
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-
 telethn = TelegramClient("Mrjoker", API_ID, API_HASH)
-
 pbot = Client("mrjoker", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
-
 #mbot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
-
 dispatcher = updater.dispatcher
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
-
 DEV_USERS = list(DEV_USERS)
-
 WOLVES = list(WOLVES)
-
 DEMONS = list(DEMONS)
-
 TIGERS = list(TIGERS)
-
 WHITELIST_USERS = list(WHITELIST_USERS)
-
 SUDO_USERS = list(SUDO_USERS)
 
 # Load at end to ensure all prev variables have been set
-
 from mrjoker.modules.helper_funcs.handlers import (
-
     CustomCommandHandler,
-
     CustomMessageHandler,
-
     CustomRegexHandler,
-
 )
 
 # make sure the regex handler can take extra kwargs
-
 tg.RegexHandler = CustomRegexHandler
-
 tg.CommandHandler = CustomCommandHandler
-
 tg.MessageHandler = CustomMessageHandler
