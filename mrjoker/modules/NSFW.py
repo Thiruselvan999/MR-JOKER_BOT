fe9b4fb5b1678d56855bc6c7180ada8829355b04
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2021-12-05 06:24:47 +0530

    Update NSFW.py

--- mrjoker/modules/NSFW.py
+++ mrjoker/modules/NSFW.py
@@ -6,7 +6,6 @@ from PIL import Image
 from telegram import ParseMode
 from mrjoker import dispatcher, updater
 import mrjoker.modules.sql.nsfw_sql as sql
-from mrjoker.modules.log_channel import gloggable
 from telegram import Message, Chat, Update, Bot, MessageEntity
 from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import CommandHandler, run_async, CallbackContext
